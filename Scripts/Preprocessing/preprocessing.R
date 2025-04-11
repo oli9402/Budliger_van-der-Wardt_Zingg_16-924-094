d53d6727b90b8ffebd2f107e8c7a3f971078e2a1
Oliver Zingg <oliver9402@gmail.com>
2025-04-11 12:49:17 +0200

    update preprocessing file with export

---
+++ Scripts/Preprocessing/preprocessing.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+# Load the data
+planes <- read.csv("./../Data/LGA.csv")
+
+# Change format and add Season, Year, Month, Weekday
+planes <- planes %>% 
+  mutate(FL_DATE = as.Date(FL_DATE)) %>% 
+  mutate(
+    Year  = format(FL_DATE, "%Y"),
+    Month = as.numeric(format(FL_DATE, "%m")),
+    Day   = format(FL_DATE, "%d")) %>% 
+  mutate(Season = case_when( 
+    Month %in% c(12,01,02) ~ "Winter" ,
+    Month %in% c(03,04,05) ~ "Spring",
+    Month %in% c(06,07,08) ~ "Summer",
+    Month %in% c(09,10,11) ~ "Fall",
+    TRUE ~ NA
+  )) %>% 
+  mutate(Weekday = weekdays(FL_DATE)) %>%
+  mutate(Weekday = fct_relevel(Weekday, "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")) %>%
+  mutate(Season = as.factor(Season)) %>% 
+  relocate(Year, Month, Day, Weekday, Season, .after = FL_DATE) 
+
+
+# Save the preprocessed data
+write.csv(planes, file = "./../Data/LGA_preprocessed.csv", row.names = FALSE)
